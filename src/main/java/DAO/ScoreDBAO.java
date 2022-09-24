73a96fb23e5ce368a691ea33ee6a148edb60bb00
CHEN HJ <87255290+CelesteAhua@users.noreply.github.com>
2022-09-24 17:33:40 +0800

    Add files via upload
    DAO包存放的各类Java文件用于实现连接数据库和后续相关功能；Entity包存放的各类Java文件用于后续实现获取每个类的具体字段信息。目前通过UserDBAO测试过可以成功连接上数据库。

---
+++ src/main/java/DAO/ScoreDBAO.java
@@ -0,0 +1,64 @@
+package DAO;
+
+import java.sql.*;
+
+public class ScoreDBAO {
+	
+	Connection con;
+    private boolean conFree = true;
+    
+    // Database configuration
+    public static String url = "jdbc:mysql://localhost:3306/gameview";
+    public static String dbdriver = "com.mysql.cj.jdbc.Driver";
+    public static String username = "root";
+    public static String password = "123456";
+    
+    public ScoreDBAO() throws Exception {
+        try {
+      
+            Class.forName(dbdriver);
+            con = DriverManager.getConnection(url, username, password);
+            System.out.println("Successfully Connected!");
+        } catch (Exception ex) {
+            System.out.println("Exception in scoreDBAO: " + ex);
+            throw new Exception("Couldn't open connection to database: " +
+                    ex.getMessage());
+        }
+    }
+    
+    
+    public void remove() {
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    protected synchronized Connection getConnection() {
+        while (conFree == false) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        
+        conFree = false;
+        notify();
+        
+        return con;
+    }
+    
+    protected synchronized void releaseConnection() {
+        while (conFree == true) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        
+        conFree = true;
+        notify();
+    } 
+
+}
