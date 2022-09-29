18315c02bedb709482cf7cc64fc127f0c29a0491
levia11 <48672309+levia11@users.noreply.github.com>
2022-09-29 16:09:52 +0800

    Update User.java

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -4,6 +4,30 @@ public class User {
 	
 	private Long id; // user id
 	
+	private String userName;
+	
+	private String password;
+	
+	private String email;
+	
+	private String phone;
+	
+	private Integer gender;
+	
+	private String dob;
+	
+	private Integer coin;
+	
+	private String following_list;
+	
+	private String fans_list;
+	
+	private String bookmark_list;
+	
+	private String token;
+	
+	private String reset_code;
+	
 	public User() {
 		
 	}
